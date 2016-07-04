5f2d70adf6e22e4e363d97507f2e3637215c03d9
marcos <marcos_alvarenga9@hotmail.com>
2016-07-04 17:19:39 -0300

    Mensaje de error en el logn y registro hechos.

--- controlDeGastos-domain/src/main/webapp/js/controllers/UsuarioController.js
+++ controlDeGastos-domain/src/main/webapp/js/controllers/UsuarioController.js
@@ -7,36 +7,22 @@ gastosApp.controller("UsuarioController", function (mainService,$state,$scope) {
                 $state.go("mainPage");                
             }
 
-        },notificarError);
+        },errorHandler);
     };
 
     $scope.registrar = function (userToRegister) {
         mainService.registrar(userToRegister, function(nuevoUsuarioRegistrado){
             if(nuevoUsuarioRegistrado !== null){
-                mainService.datosDelUsuario(nuevoUsuarioRegistrado);
+                mainService.datosDelUsuario(nuevoUsuarioRegistrado.data);
                 $state.go("mainPage");
+                console.log(nuevoUsuarioRegistrado.data);
             }
 
-        },notificarError);
+        },errorHandler);
     };    
 
-
-    /*  revisar las paginas sobre como inyectar dependencias necsarias
-    $scope.showAlert = function(mensaje) {
-        $mdDialog.show(
-            $mdDialog.alert()
-                .parent(angular.element(document.querySelector('#popupContainer')))
-                .clickOutsideToClose(true)
-                .title('Error al ingresar')
-                .textContent(mensaje)
-                .ariaLabel('Alert Dialog Demo')
-                .ok('Got it!')
-        );
-    };*/
-
-    function notificarError(mensaje) {
-        //this.showAlert(mensaje.data);
-        console.log(mensaje.data);
+    function errorHandler(error) {
+        $scope.mostrarError = error.data;
     }
 
 });
